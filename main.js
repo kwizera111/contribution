f7625edcf93255530efb685e7e12a9c9906b7d08
kwizera edison <kwizeraedison6@gmail.com>
2025-08-14 12:36:02 +0200

    second changes have been made

--- main.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('input');
-const result = document.getElementById('result');
-
-function output() {
-    window.alert("You searched for: " + input.value);
-    input.value = ""; // Clear the input field after alert
-}
